872f3677cad52ec6a419ec8f96a9dc426ba33f71
Aneri12345 <57945364+Aneri12345@users.noreply.github.com>
2022-04-24 12:10:30 +0530

    Added codes for Trained models

---
+++ Codes/LGBM.py
@@ -0,0 +1,34 @@
+import optuna.integration.lightgbm as lgb
+
+from lightgbm import early_stopping
+from lightgbm import log_evaluation
+import sklearn.datasets
+from sklearn.model_selection import KFold
+
+
+if __name__ == "__main__":
+    data, target = sklearn.datasets.load_breast_cancer(return_X_y=True)
+    dtrain = lgb.Dataset(data, label=target)
+
+    params = {
+        "objective": "binary",
+        "metric": "binary_logloss",
+        "verbosity": -1,
+        "boosting_type": "gbdt",
+    }
+
+    tuner = lgb.LightGBMTunerCV(
+        params,
+        dtrain,
+        folds=KFold(n_splits=3),
+        callbacks=[early_stopping(100), log_evaluation(100)],
+    )
+
+    tuner.run()
+
+    print("Best score:", tuner.best_score)
+    best_params = tuner.best_params
+    print("Best params:", best_params)
+    print("  Params: ")
+    for key, value in best_params.items():
+        print("    {}: {}".format(key, value))
